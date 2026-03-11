cd280d64c08a16aee0e781fae2472b7ff865317c
viktor <viktorbol1984@gmail.com>
2026-03-11 16:11:40 +0200

    class hospital home work 1.2.0.1 - base forms

--- cash_flow/__manifest__.py
+++ cash_flow/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Cash Flow",
-    "summary": "Cash flow accounting",
+    "summary": "Cash Flow (Payment Calendar)",
     "version": "19.0.1.0.0",
     "category": "Accounting",
     "author": "viktorbol1984",
@@ -9,7 +9,12 @@
     "depends": ["base", "base_address_extended"],
     "data": [
         "security/ir.model.access.csv",
-        "views/cash_flow_views.xml",
+        "views/cash_flow_menu.xml",
+        "views/cash_flow_cashbox_views.xml",
+        "views/cash_flow_transaction_views.xml",
+        "views/cash_flow_dds_article_views.xml",
+        "views/cash_flow_dds_category_views.xml",
+        "views/cash_flow_transfer_views.xml",
         "wizard/cash_flow_forecast_wizard_views.xml",
         "views/res_partner_views.xml",
         "views/res_users_views.xml",
